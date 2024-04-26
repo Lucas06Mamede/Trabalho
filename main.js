14ddd17e5dce3568dc2672d8171cef9489fd1d7e
Lucas06Mamede <168151586+Lucas06Mamede@users.noreply.github.com>
2024-04-26 10:52:36 -0300

    main.js

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+const tempoObjetivo1 = new Date("2024-07-09T00:00:00");
+const tempoObjetivo2 = new Date("2024-05-20T00:00:00");
+const tempoObjetivo3 = new Date("2024-08-15T00:00:00");
+const tempoObjetivo4 = new Date("2026-03-25T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
@@ -40,7 +40,6 @@ function calculaTempo(tempoObjetivo) {
         return [0,0,0,0];
     }
 }
-
 function atualizaCronometro(){
     for (let i=0; i<contadores.length;i++){
         document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
@@ -49,10 +48,8 @@ function atualizaCronometro(){
         document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];   
     }
 }
-
 function comecaCronometro(){
     atualizaCronometro();
     setInterval(atualizaCronometro,1000);
 }
-
 comecaCronometro();
